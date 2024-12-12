f75ee3c199ae84fcbbc32e7b67d8dd53766dd86f
yujin <yuyujinkim65@gmail.com>
2024-12-12 17:23:14 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17720 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        String id = "";
+        id = scan.nextLine();
+        
+        System.out.print(id + "??!");
+    }
+}
