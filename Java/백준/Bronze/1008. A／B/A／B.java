ef4c2ab79e19ea297ef50cd250d0aa88743c066c
yujin <yuyujinkim65@gmail.com>
2024-12-12 16:56:57 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        double a = scan.nextInt();
+        double b = scan.nextInt();
+        
+        System.out.print(a/b);
+    }
+}
