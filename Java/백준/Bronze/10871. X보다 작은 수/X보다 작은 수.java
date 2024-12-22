dd197ec0f1e523655044802e7886013ae6efefb8
yujin <yuyujinkim65@gmail.com>
2024-12-22 20:17:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 288 ms, Memory: 17504 KB -BaekjoonHub

--- Java/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ Java/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,27 +1,31 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = scan.nextInt();	//정수 N개
-		int X = scan.nextInt();	//정수 X
-		int A[] = new int[N];	//수열 A
+		int N = Integer.parseInt(st.nextToken()); //정수 N개
+		int X = Integer.parseInt(st.nextToken()); //정수 X
 		
+		//StringTokenizer: 공백이 있으면 뒤에 문자열이 공백 자리로
+		st = new StringTokenizer(br.readLine(), " "); 
+		
+		//숫자 입력, X보다 작은 숫자 출력
 		for (int i=0; i<N; i++) {
-			A[i] = scan.nextInt();	//정수N만큼 정수 입력
+			int number = Integer.parseInt(st.nextToken());
+			
+			if(number < X) {
+				System.out.print(number + " ");
+			}//if
+
 		}//for
 		
-		// 정수 X보다 작은 정수 출력
-		for (int i=0; i<A.length; i++) {
-			if (A[i] < X) {
-				System.out.print(A[i] + " ");
-			}//if
-		}//for
-	
-	}//main
+	}// main
 
-}//class
+}// class
